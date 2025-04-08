f793ac401552711b6f9d98f314769b77064078e9
kennybowen <kennyoluwadamilare20@gmail.com>
2025-04-08 14:07:42 +0100

    implements sidebar layout for the home page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,16 @@
+import Sidebar from "@/components/Sidebar";
+
 export default function RootLayout({
   children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: "Kenny", lastName: "Kay" };
   return (
-    <main>
-      SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+
+      <div className="flex size-full flex-col"></div>
       {children}
     </main>
   );
