fa9911ca5195a2635de6186b0b02f4fcfff78f6c
kennybowen <kennyoluwadamilare20@gmail.com>
2025-04-08 13:04:46 +0100

    implement charts, account balance, and download charts packages

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,19 +1,26 @@
 import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
   const loggedIn = { firstName: "Kenny" };
 
   return (
-    <section className="home no-scrollbar">
-      <div className="home-content no-scrollbar">
+    <section className="home">
+      <div className="home-content">
         <header className="home-header">
           <HeaderBox
             type="greeting"
             title="Welcome"
             user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
           />
         </header>
+        <TotalBalanceBox
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={1250.35}
+        />
       </div>
     </section>
   );
--- components/HeaderBox.tsx
+++ components/HeaderBox.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 
-const HeaderBox = () => {
-  return <div>HeaderBox</div>;
+const HeaderBox = ({
+  type = "title",
+  title,
+  subtext,
+  user
+}: HeaderBoxProps) => {
+  return (
+    <div className="header-box">
+      <h1 className="header-box-title">
+        {title}
+        &nbsp;
+        {type === "greeting" && (
+          <span className="text-bankGradient">{user}</span>
+        )}
+      </h1>
+      <p className="header-box-subtext">{subtext}</p>
+    </div>
+  );
 };
 
 export default HeaderBox;
